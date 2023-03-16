4c2fbf649bcbcb8c902de16d0a4309ceb13ab6ba
Paul Miller <paul@paulmillr.com>
2023-03-16 18:56:43 +0000

    Bump deps, move build files

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'index.js',
+  input: '../index.js',
   output: {
     file: 'build/micro-eth-signer.js',
     format: 'umd',
