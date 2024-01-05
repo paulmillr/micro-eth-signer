c4078b7bfb7596b1bd142cc11a4b8ff82de06036
Paul Miller <paul@paulmillr.com>
2024-01-05 05:06:38 +0000

    Refactor tests

--- test/ens.test.js
+++ test/ens.test.js
@@ -1,17 +1,17 @@
 import { deepStrictEqual } from 'node:assert';
-import { should } from 'micro-should';
+import { describe, should } from 'micro-should';
 import { hex } from '@scure/base';
 import { namehash } from '../lib/esm/api/ens.js';
 
-should('ENS: namehash', () => {
-  deepStrictEqual(
-    hex.encode(namehash('vitalik.eth')),
-    'ee6c4522aab0003e8d14cd40a6af439055fd2577951148c14b6cea9a53475835'
-  );
-  deepStrictEqual(
-    hex.encode(namehash('benjaminion.eth')),
-    'ce1ee36a55b52d39db63e16d1a097df75b04ede734494425de534e1b3f97d221'
-  );
+describe('ENS', () => {
+  should('namehash', () => {
+    deepStrictEqual(
+      hex.encode(namehash('vitalik.eth')),
+      'ee6c4522aab0003e8d14cd40a6af439055fd2577951148c14b6cea9a53475835'
+    );
+    deepStrictEqual(
+      hex.encode(namehash('benjaminion.eth')),
+      'ce1ee36a55b52d39db63e16d1a097df75b04ede734494425de534e1b3f97d221'
+    );
+  });
 });
-
-
