a4732cce85d39f0f0525f24e377bc2b5e9da88ba
Paul Miller <paul@paulmillr.com>
2024-12-06 19:19:30 +0000

    Update build

--- build/input.js
+++ build/input.js
@@ -1,1 +1,8 @@
-export { addr, Transaction } from 'lib';
+import {
+  amounts, ethHex, ethHexNoLeadingZero, weieth, weigwei,
+} from 'micro-eth-signer/utils';
+export { addr, Transaction } from 'micro-eth-signer';
+export const utils = {
+  amounts, ethHex, ethHexNoLeadingZero, weieth, weigwei,
+};
+export * as typedData from 'micro-eth-signer/typed-data';
